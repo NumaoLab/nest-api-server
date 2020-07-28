28a9efb45078b62a92bf1ca9fba35f56c179443d
Kinnosuke Tanaka <kt@MacBook-Pro-2.local>
2020-07-27 19:14:45 -0700

    Fix conf

--- server/minio-client/minio.config.prod.ts
+++ server/minio-client/minio.config.prod.ts
@@ -2,7 +2,7 @@ import { MinioOptions } from 'nestjs-minio-client/dist/interfaces/minio.options.
 
 export const uri = "localhost";
 export const bucket = "test";
-export const port = 9000;
+export const port = 9001;
 export const options: MinioOptions = {
   endPoint: uri,
   port: 9000,
