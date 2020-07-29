2bd542da7ae7e03459fddc5576e84ecd05c43018
Kinnosuke Tanaka <kt@MacBook-Pro-2.local>
2020-07-28 18:24:16 -0700

    Fix port

--- server/minio-client/minio.config.prod.ts
+++ server/minio-client/minio.config.prod.ts
@@ -2,12 +2,11 @@ import { MinioOptions } from 'nestjs-minio-client/dist/interfaces/minio.options.
 
 export const uri = "minio";
 export const bucket = "mimamori";
-export const port = 9001;
+export const port = 9000;
 export const options: MinioOptions = {
   endPoint: uri,
   port: port,
   useSSL: false,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
-  signatureVersion: 'v4',
 };
